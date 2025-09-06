c21c98f8bc3fc3d2dcfc5ea483ab7ff562021438
charitha36 <dcsri2024@gmail.com>
2025-09-06 17:12:34 +0530

    main,math,asyncawait

---
+++ main.js
@@ -0,0 +1,8 @@
+import {
+    add,multiply,sub
+}from "./math.js"
+import divide from "./math.js"
+console.log(add(2,5))
+console.log(multiply(2,5))
+console.log(sub(2,5))
+console.log(divide(10,5))
---
+++ math.js
@@ -0,0 +1,17 @@
+export const add=function (x,y){
+
+    return x+y;
+}
+
+export const multiply =function(x,y){
+    return x*y;
+}
+ export const sub=function (x,y){
+    return x-y;
+}
+
+const divide=(x,y)=>{
+    return x/y;
+}
+export default divide;
+
