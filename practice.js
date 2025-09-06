b1023706ddddcb5e8b33c874b011a616f3e037cb
charitha36 <dcsri2024@gmail.com>
2025-09-06 12:35:54 +0530

    learn datatype

--- practice.js
+++ practice.js
@@ -1,1 +1,27 @@
 console.log("hello,world!,charitha")
+let name = "Charitha";
+console.log(name)
+let num=18;
+console.log(num)
+let islogged=false;
+console.log(islogged)
+let x;
+console.log(x)
+let y=null;
+console.log(y)
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
+console.log(typeof x)
+console.log(typeof y)
+
+let arr=[1,2,3,4]
+console.log(arr[0])
+console.log(typeof arr)
+let person={
+    name:"charitha",
+    age:18
+}
+console.log(person.age)
+console.log(person.name)
+console.log(typeof person)
