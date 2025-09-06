5d0f477ac3a3c2353200f1cdd4703756ee078681
charitha36 <dcsri2024@gmail.com>
2025-09-06 15:54:09 +0530

    world

---
+++ functions.js
@@ -0,0 +1,27 @@
+function greet(){
+    console.log("Welcome to DevAstra")
+}
+greet()
+
+function add(x,y){
+    return x+y;
+}
+let result=add(2,5)
+console.log(result)
+
+const pi=3.14
+console.log(pi)
+
+const multiply =function(x,y){
+    return x*y;
+}
+console.log(multiply(5,3))
+
+//Arrow function
+const square =(x)=>x*x;
+console.log(square(5))
+const subtract=(x,y)=>{
+    console.log(x,y)
+    return x-y;
+}
+ console.log(subtract(7,5))
